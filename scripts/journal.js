69b68c1e7ba9a2e79529806b8b9ff063ca02b9c2
LanceP51 <appleguy05@live.com>
2019-08-14 15:49:21 -0400

    first upload of beginning of daily journals

---
+++ scripts/journal.js
@@ -0,0 +1,23 @@
+myJournalEntries=[]
+var entryAug12 = {
+    date: "Aug 12",
+    concepts: "javascript basics",
+    entry: "re-covering topics from on-ramp and removing any barriers",
+    mood: "Successful"
+}
+var entryAug9 = {
+    date: "Aug 9",
+    concepts: "html and css",
+    entry: "beautifying my website and learning about bootstrap",
+    mood: "Interested"
+}
+var entryAug8 = {
+    date: "Aug 8",
+    concepts: "html and css",
+    entry: "learning aspects of css to help improve custom look of webpages",
+    mood: "Successful"
+}
+myJournalEntries.push(entryAug8)
+myJournalEntries.push(entryAug9)
+myJournalEntries.push(entryAug12)
+console.log(myJournalEntries)
